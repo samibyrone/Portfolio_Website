dd6ace9667eaad69e18e7f964f94a1bc15a4b089
samibyrone <samsonibironke2@gmail.com>
2025-02-01 23:59:38 +0100

    portfolio setup more updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+{import('tailwindcss').config}
+
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+         "primary": "#24262b",
+         "primary-gray": "#5e626f;",
+         "primary-green": "#0D775E;",
+         "red-gray": "#444444",
+         "white-100": "#F3F3F3",
+      },
+    },
   },
   plugins: [],
-}
-
+};
